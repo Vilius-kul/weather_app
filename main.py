9e43743dc1df63eb70ff7355e3b00f4e1d6fa2af
Vilius Kulinas <Vilius.kulinas@gmail.com>
2023-06-23 15:28:49 +0100

    add configure function and refactored so that it would autoload than run uvicorn main:app --reload

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from views import home
 from api import weather_api
 
 api = fastapi.FastAPI()
-api.include_router(home.router)
-api.include_router(weather_api.router)
 
 
 
+def configure():
+    configure_routing()
+
+def configure_routing():
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
 
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
+else:
+     configure()
